10bb6fc57820cd7362bbb85436f43b11b4e04752
Wenfeng Wang <kalot.wang@gmail.com>
2022-03-08 15:18:58 +0800

    Mint request parse

--- build.rs
+++ build.rs
@@ -1,10 +1,23 @@
 extern crate protoc_rust;
+use std::env;
+use std::fs;
+use std::path::Path;
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap_or(String::from("src/binding/protos"));
+    if Path::new(&out_dir).exists() {
+        fs::remove_dir_all(&out_dir).unwrap();
+    }
+    fs::create_dir(&out_dir).unwrap();
+
     protoc_rust::Codegen::new()
-        .out_dir("src/binding/protos")
+        .customize(protoc_rust::Customize {
+            gen_mod_rs: Some(true),
+            ..Default::default()
+        })
+        .out_dir(out_dir)
         .include("src/protos")
-        .inputs(&["src/protos/pegged.proto"])
+        .inputs(&["src/protos/rustproto.proto", "src/protos/pegged.proto"])
         .run()
         .expect("Running protoc failed.");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod binding;
+
+use protobuf::Message;
+use binding::protos::pegged;
+
+
 fn main() {
-    println!("Hello, world!");
+    // Mint
+    let mut mint_request = pegged::Mint::new();
+    mint_request.token = b"PHA".to_vec();
+    mint_request.account = b"Alice".to_vec();
+    mint_request.amount = b"10000".to_vec();
+    mint_request.depositor = b"Bob".to_vec();
+    mint_request.ref_chain_id = 1;
+    mint_request.ref_id = b"ref id".to_vec();
+
+    let out_bytes: Vec<u8> = mint_request.write_to_bytes().unwrap();
+
+    // Decode mint request
+    let decoded_request = pegged::Mint::parse_from_bytes(&out_bytes).unwrap();
+
+    assert_eq!(decoded_request.token, b"PHA".to_vec());
+    print!("Mint request parse succeed.");
 }
